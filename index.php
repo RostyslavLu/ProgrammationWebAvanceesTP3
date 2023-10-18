bef4498829e3b3f06673f2d73fab56ca02c3dae4
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2023-10-18 16:29:27 -0400

    logbook

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+define('PATH_DIR', 'http://localhost:80/webAvancee22645/ProgrammationWebAvanceesTP3/');
+require_once(__DIR__.'/controller/Controller.php');
+require_once(__DIR__.'/library/View.php');
+require_once(__DIR__.'/library/RequirePage.php');
+require_once(__DIR__.'/vendor/autoload.php');
+require_once(__DIR__.'/library/Twig.php');
+require_once(__DIR__.'/library/CheckSession.php');
+
+$url = isset($_GET["url"]) ? explode ('/', ltrim($_GET["url"], '/')) : '/';
+//echo $url;
+//print_r($url)
+if ($url == '/') {
+
+    $controllerHome = __DIR__.'/'.'controller/ControllerHome.php';
+    require_once $controllerHome;
+    $controller = new ControllerHome;
+    echo $controller->index();
+    
+
+} else {
+
+    $requestURL = $url[0];
+    $requestURL = ucfirst($requestURL);
+    $controllerPath = __DIR__.'/'.'controller/Controller'.$requestURL.'.php';
+
+    if (file_exists($controllerPath)) {
+
+        //echo $controllerPath;
+        require_once ($controllerPath);
+        $controllerName = 'Controller'.$requestURL;
+        $controller = new $controllerName;
+        if (isset($url[1])) {
+            $method = $url[1];
+            
+            if (isset($url[2])) {
+                $id = $url[2];
+                echo $controller->$method($id);
+            } else {
+                echo $controller->$method();
+            }
+        } else {
+            echo $controller->index();
+        }
+        
+
+    } else {
+
+        $controllerHome = __DIR__.'/'.'controller/ControllerHome.php';
+        require_once $controllerHome;
+        $controller = new ControllerHome;
+        echo $controller->error();
+
+    }
+}
+
+
+?>
